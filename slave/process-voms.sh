81895af238d442561ab00ad437c21d582b3e2d13
Zdenek Sustr <git@sustr.net>
2015-09-25 13:10:39 +0200

    Processing all returns from voms-admin

--- slave/process-voms.sh
+++ slave/process-voms.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Script for managing user membership in VOs
+# Return values:
+# 0 ... OK
+# 1 ... Single user issue
+# 2 ... VO-wide issue
+# 3 ... VO-availability issue (VO does not exist or is inaccessible)
+
 PROTOCOL_VERSION='3.0.0'
 
 function process {
@@ -13,12 +20,15 @@ function process {
 
 	FROM_PERUN="${WORK_DIR}/voms"
 
+	RETVAL=0
+
 	create_lock
 
 	VO_USERS=`mktemp`
 	CURRENT_VO_USERS=`mktemp`
 	CURRENT_VO_RAW_USERS=`mktemp`
 	CAS=`mktemp`
+	USER_RESPONSE=`mktemp`
 
 	# Get list of VOs
 	VOS=`cat ${FROM_PERUN} | sed 's/^\([[:alnum:]_.-]*\)\t.*/\1/' | uniq`
@@ -31,16 +41,18 @@ function process {
 		# Get current users stored in VOMS and convert lines into PERUN format. VOMS also doesn't accept emailAddress in the DN, it must be converted to Email
 		voms-admin --vo "${VO}" list-users > ${CURRENT_VO_RAW_USERS}
 		if [ $? -ne 0 ]; then
-			printf "WARNING: VO \"${VO}\" does not exist or is inactive. Original message from voms-admin:\n\t\"`cat ${CURRENT_VO_RAW_USERS}`\"\n"
+#			logger "Perun:VO \"${VO}\" does not exist or is inactive. Original message from voms-admin: \"`cat ${CURRENT_VO_RAW_USERS}`\""
+			RETVAL=3
 			continue
 		fi
 
 		cat ${CURRENT_VO_RAW_USERS} | sed "s/\(.*\),\(.*\),\(.*\)/${VO}\t\1\t\2\t\3/" | sed 's/emailAddress/Email/' | sort > ${CURRENT_VO_USERS}
 
 		# Get list of accepted CAs
-		voms-admin --vo "${VO}x" list-cas > ${CAS}
+		voms-admin --vo "${VO}" list-cas > ${CAS}
 		if [ $? -ne 0 ]; then
-			printf "WARNING: Failed getting accepted CAs for VO \"${VO}\". Original message from voms-admin:\n\t\"`cat ${CAS}`\"\n"
+#			logger "Perun:Failed getting accepted CAs for VO \"${VO}\". Original message from voms-admin: \"`cat ${CAS}`\""
+			if [ $RETVAL -lt 2 ]; then RETVAL=2; fi
 			continue
 		fi
 
@@ -51,7 +63,12 @@ function process {
 				echo -e "$CURRENT_VO_USER" | while IFS=`echo -ne "\t"` read VO_SHORTNAME USER_DN CA_DN USER_EMAIL; do
 					# Check if the user's certificate was issued by accepted CA
 					if [ `grep -c "$CA_DN" $CAS` -gt 0 ]; then
-						voms-admin --nousercert --vo "${VO_SHORTNAME}" delete-user "$USER_DN" "$CA_DN"
+						voms-admin --nousercert --vo "${VO_SHORTNAME}" delete-user "$USER_DN" "$CA_DN" > ${USER_RESPONSE}
+						if [ $? -ne 0 ]; then
+							logger "Perun:Failed removing user from VO \"${VO}\". Original message from voms-admin: \"`cat ${USER_RESPONSE}`\""
+							if [ $RETVAL -lt 1 ]; then RETVAL=1; fi
+							continue
+						fi
 						logger Perun:VOMS user $USER_DN - $CA_DN removed from ${VO_SHORTNAME}
 					fi
 				done
@@ -68,7 +85,12 @@ function process {
 					# Check if the user's certificate was issued by accepted CA
 					if [ `grep -c "$CA_DN" $CAS` -gt 0 ]; then
 						USER_CN=`echo $USER_DN | sed 's|^.*\/CN=\([^/]*\).*|\1|'`
-						voms-admin --nousercert --vo "${VO_SHORTNAME}" create-user "$USER_DN" "$CA_DN" "$USER_CN" "$USER_EMAIL"
+						voms-admin --nousercert --vo "${VO_SHORTNAME}" create-user "$USER_DN" "$CA_DN" "$USER_CN" "$USER_EMAIL" > ${USER_RESPONSE}
+						if [ $? -ne 0 ]; then
+							logger "Perun:Failed adding user to VO \"${VO}\". Original message from voms-admin: \"`cat ${USER_RESPONSE}`\""
+							if [ $RETVAL -lt 1 ]; then RETVAL=1; fi
+							continue
+						fi
 						logger Perun:VOMS user $USER_DN - $CA_DN added to ${VO_SHORTNAME}
 					fi
 				done
@@ -80,4 +102,7 @@ function process {
 	rm -f $CURRENT_VO_USERS
 	rm -f $CURRENT_VO_RAW_USERS
 	rm -f $CAS
+	rm -f $USER_RESPONSE
+
+	exit $RETVAL
 }
