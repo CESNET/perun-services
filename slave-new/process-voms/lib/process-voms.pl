f75cd2ad2b164de1406e725f599afe124a5a4f51
Zdenek Sustr <sustr4@cesnet.cz>
2016-01-25 14:48:17 +0100

    New perl-based slave, mostly a placeholder

---
+++ slave-new/process-voms/lib/process-voms.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+
+# Potentially useful VOMS links
+#https://voms2.cern.ch:8443/voms/alice/services/VOMSAdmin?method=listSubGroups
+#https://voms2.cern.ch:8443/voms/alice/services/VOMSAdmin?method=listSubGroups&groupname=/alice/alarm
+#https://voms2.cern.ch:8443/voms/alice/services/VOMSAdmin?method=listMembers&groupname=/alice/alarm
+#https://voms2.cern.ch:8443/voms/alice/services/VOMSAdmin?method=listRoles
+
