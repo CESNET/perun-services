16a786d29a180e808474609d1f0e39db5b515ee2
Zdenek Sustr <sustr4@cesnet.cz>
2016-01-28 12:06:14 +0100

    Reading essential data from export file

--- slave-new/process-voms/lib/process-voms.pl
+++ slave-new/process-voms/lib/process-voms.pl
@@ -6,3 +6,20 @@
 #https://voms2.cern.ch:8443/voms/alice/services/VOMSAdmin?method=listMembers&groupname=/alice/alarm
 #https://voms2.cern.ch:8443/voms/alice/services/VOMSAdmin?method=listRoles
 
+# initialize parser and read the file
+use XML::Simple;
+use Data::Dumper;
+$vos = XMLin( '-' );
+
+#print(Dumper($vos));
+
+printf "-------\n";
+
+# serialize the structure
+foreach my $name (keys $vos->{'vo'}) {
+	$vo=$vos->{'vo'}->{$name};
+	printf "${name}\n";
+	foreach my $user (values $vo->{'users'}->{'user'}) {
+		printf "\t\"%s\" \"%s\"\n",$user->{'DN'},$user->{'CA'};
+	}
+}
