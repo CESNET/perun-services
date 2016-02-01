dc6189613db610da1c11e2710a81e3be80483649
Zdenek Sustr <sustr4@cesnet.cz>
2016-02-01 14:27:10 +0100

    Reading basic info from VO

--- slave-new/process-voms/lib/process-voms.pl
+++ slave-new/process-voms/lib/process-voms.pl
@@ -10,12 +10,35 @@ $vos = XMLin( '-' );
 # Main parsing loop for the input XML file
 foreach my $name (keys %{$vos->{'vo'}}) { # Iterating through individual VOs in the XML
 	$vo=$vos->{'vo'}->{$name};
+	printf "---\nVO:\t${name}\n";
 
-	printf "${name}\n";
+	@groups_current=`voms-admin --vo ${name} list-groups`;
+	chomp(@groups_current);
+	s/^\s*// for @groups_current;
+
+	@roles_current=`voms-admin --vo ${name} list-roles`;
+	chomp(@roles_current);
+	s/^\s*Role=// for @roles_current;
+
+	@users_current=`voms-admin --vo ${name} list-users`;
+	chomp(@users_current);
+	print "Roles:";
+	foreach $role (@roles_current) {
+		printf "\t%s\n",$role;
+	}
+	print "Groups:";
+	foreach $group (@groups_current) {
+		printf "\t%s\n",$group;
+	}
+	print "Users:";
+	foreach $user (@users_current) {
+		printf "\t%s\n",$user;
+	}
+
 	#Collect all lists from voms-admin
 
 #	print(Dumper($vo));
 	foreach my $user (@{$vo->{'users'}->{'user'}}) {
-		printf "\t\"%s\" \"%s\"\n",$user->{'DN'},$user->{'CA'};
+#		printf "\t\"%s\"\n\t\t\"%s\"\n",$user->{'DN'},$user->{'CA'};
 	}
 }
