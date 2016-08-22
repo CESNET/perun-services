8f1376f552f7fbe02dab27d04f1db9e2d371e3c4
Pavel Zlámal <zlamal@cesnet.cz>
2016-08-22 17:55:24 +0200

    Return $lock from SendUtils back to script
    - SendUtils.pm is now more like static library.
    - Use new way in all relevant send scripts.

--- send/SendUtils.pm
+++ send/SendUtils.pm
@@ -7,9 +7,7 @@ use Fcntl qw(:flock);
 # BEGIN {push @INC, '/dir/for/perl/lib'}
 # use SendUtils;
 # SendUtils::lock_file("Name");
-# SendUtils::unlock_file();
-
-my $lock;
+# SendUtils::unlock_file($lock);
 
 #
 # Create a lock file of specified name and keeps it locked
@@ -19,6 +17,9 @@ my $lock;
 # In case of process/system failiure lock file is unlocked by the OS.
 #
 sub lock_file {
+
+	my $lock;
+
 	my ($lockDirName) = @_;
 	unless(defined($lockDirName)) { die "Missing log dir name - $!\n"; }
 
@@ -30,12 +31,16 @@ sub lock_file {
 	open($lock, ">", $lockPidFile) or die "Can't open lock file: $!";
 	flock($lock, LOCK_EX|LOCK_NB) or die "Cannot create a lock, probably already locked - $!\n";
 	print $lock $$;
+
+	return $lock;
+
 }
 
 #
 # Unlock the lock file created by the call lock_file
 #
 sub unlock_file {
+	my ($lock) = @_;
 	unless(defined($lock)) { die "Can't unlock file, lock was not created!"; }
 	flock($lock, LOCK_UN|LOCK_NB) or die "Cannot unlock file - $!\n";
 }
