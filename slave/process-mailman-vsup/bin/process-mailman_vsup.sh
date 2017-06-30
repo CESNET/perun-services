cd4ffb415019a55ae7651d1e397f33691f9ccf45
Pavel Zlámal <zlamal@cesnet.cz>
2017-06-30 13:03:41 +0200

    New service mailman_vsup
    - Modification of generic mailman service for VŠUP.
      It allows management of all mailinglists sent from perun.

---
+++ slave/process-mailman-vsup/bin/process-mailman_vsup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+PROTOCOL_VERSION='3.1.0'
+
+function process {
+
+	FROM_PERUN_DIR="${WORK_DIR}/mailinglists/"
+
+	I_MAILING_LIST_UPDATED=(0 '${MAILING_LIST_NAME} successfully updated.')
+
+	create_lock
+
+	for MAILING_LIST_NAME in `ls $FROM_PERUN_DIR/` ; do
+			# set list members
+			cat "${FROM_PERUN_DIR}/${MAILING_LIST_NAME}" | grep -v "^#" | sudo /usr/local/mailman/bin/sync_members --welcome-msg=no --goodbye-msg=no -f - $MAILING_LIST_NAME
+			log_msg I_MAILING_LIST_UPDATED
+	done
+
+}
