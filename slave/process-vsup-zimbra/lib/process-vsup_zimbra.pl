7b87076c4c62df240cbb1167ba255399d826d5c7
Pavel Zlámal <zlamal@cesnet.cz>
2017-09-26 11:00:00 +0200

    Prevent printing of perl warnings in vsup_zimbra service

--- slave/process-vsup-zimbra/lib/process-vsup_zimbra.pl
+++ slave/process-vsup-zimbra/lib/process-vsup_zimbra.pl
@@ -242,23 +242,24 @@ sub compareAndUpdateAttribute() {
 	my $zimbraAccount = shift;
 	my $attrName = shift;
 
-	if ($perunAccount->{$attrName} ne $zimbraAccount->{$attrName}) {
+	# Make sure comparison and print work
+	my $perunAttr = ($perunAccount->{$attrName}) ? $perunAccount->{$attrName} : '';
+	my $zimbraAttr = ($zimbraAccount->{$attrName}) ? $zimbraAccount->{$attrName} : '';
 
-		# Zimbra can have uninitialized string value !! - breaks print
-		my $zmval = $zimbraAccount->{$attrName};
-		if (!defined $zmval) { $zmval = ''; }
+	if ($perunAttr ne $zimbraAttr) {
 
 		if ($dry_run) {
-			print $perunAccount->{"MAILBOX"} . " $attrName would be updated '$zmval'=>'$perunAccount->{$attrName}'.\n";
-			logMessage($perunAccount->{"MAILBOX"} . " $attrName would be updated '$zmval'=>'$perunAccount->{$attrName}'.");
+			print $perunAccount->{"MAILBOX"} . " $attrName would be updated '$zimbraAttr'=>'$perunAttr'.\n";
+			logMessage($perunAccount->{"MAILBOX"} . " $attrName would be updated '$zimbraAttr'=>'$perunAttr'.");
 		} else {
+			# user original value in update call
 			my $ret = updateAccount($perunAccount->{"MAILBOX"}, $attrName, $perunAccount->{$attrName});
 			if ($ret != 0) {
 				print "ERROR: " . $perunAccount->{"MAILBOX"} . " update of $attrName failed.\n";
 				logMessage("ERROR: " . $perunAccount->{"MAILBOX"} . " update of $attrName failed, ret.code: " . $ret);
 			} else {
-				print $perunAccount->{"MAILBOX"} . " $attrName updated '$zmval'=>'$perunAccount->{$attrName}'.\n";
-				logMessage($perunAccount->{"MAILBOX"} . " $attrName updated '$zmval'=>'$perunAccount->{$attrName}'.\n");
+				print $perunAccount->{"MAILBOX"} . " $attrName updated '$zimbraAttr'=>'$perunAttr'.\n";
+				logMessage($perunAccount->{"MAILBOX"} . " $attrName updated '$zimbraAttr'=>'$perunAttr'.\n");
 			}
 		}
 	}
