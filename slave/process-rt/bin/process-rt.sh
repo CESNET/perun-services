4e0b157701a1220ef64b2f8cdc21bb8194544f40
Michal Stava <stavamichal@gmail.com>
2019-08-09 08:49:49 +0200

    Changes in service 'rt'
     - new funcionality for changing name of output file was added
     - the name of output file is now part of the service set to the
     specific file 'output_file_name', then read from this file and used for
     saving file with data to '/tmp/{fileName}' location. Default is still
     '/tmp/rt-data' if the facility attribute with file name is empty.
     - protocol version was increased
     - timestamp in changelog was fixed

--- slave/process-rt/bin/process-rt.sh
+++ slave/process-rt/bin/process-rt.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
-PROTOCOL_VERSION='3.0.0'
-
+PROTOCOL_VERSION='3.1.0'
 
 function process {
-	DST_FILE="/tmp/rt-data"
+	DST_DIR="/tmp/"
 
 	### Status codes
 	I_CHANGED=(0 "${DST_FILE} updated")
 	E_NOT_CHANGE=(50 'Cannot copy file ${FROM_PERUN} to ${DST_FILE}')
+	E_FILE_NOT_EXIST=(51 'Cannot find file with destination file name in ${NAME_FILE}')
 
 	FROM_PERUN="${WORK_DIR}/rt"
+	NAME_FILE="${WORK_DIR}/output_file_name"
+
+	if [ ! -s "$NAME_FILE" ]; then
+		log_msg E_FILE_NOT_EXIST
+	fi
+	DST_FILE=`cat $NAME_FILE`
 
 	create_lock
 
-	cp "${FROM_PERUN}" "${DST_FILE}"
+	cp "${FROM_PERUN}" "${DST_DIR}/${DST_FILE}"
 
 	if [ $? -eq 0 ]; then
 		log_msg I_CHANGED
 	else
-		log_msg I_NOT_CHANGED
+		log_msg E_NOT_CHANGED
 	fi
 }
