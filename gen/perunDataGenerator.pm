b3deba2633f5d51e9dcd6a273820c45dfcafe9f7
Radoslav Čerhák <r.cerhak@gmail.com>
2021-03-15 14:25:10 +0100

    Services afs*, affiliation*, apache* edited to use hashed data
    - Services afs, afs_group, apache_basic_auth, apache_shibboleth,
    apache_ssl, affiliation_mapping and affiliation_mapping_mu are now using
    getHashedHierarchicalData.
    - Services affiliation_mapping and affiliation_mapping_mu use subroutine
    generateUsersDataInJSON from perunDataGenerator, which was edited to use
    hashed data as well.

--- gen/perunDataGenerator.pm
+++ gen/perunDataGenerator.pm
@@ -18,18 +18,58 @@ sub generateUsersDataInJSON {
 	perunServicesInit::init;
 
 	my $DIRECTORY = perunServicesInit::getDirectory;
-	my $data = perunServicesInit::getFlatData;
+	my $data = perunServicesInit::getHashedHierarchicalData;
+	my $agent = perunServicesInit->getAgent;
+	my $attributesAgent = $agent->getAttributesAgent;
+	my $servicesAgent = $agent->getServicesAgent;
+	my $service = $servicesAgent->getServiceByName( name => $::SERVICE_NAME);
+
+	my @requiredAttributesDefinitions = $attributesAgent->getRequiredAttributesDefinition(service => $service->getId);
+	my @userRequiredAttributes = ();
+	my @userFacilityRequiredAttributes = ();
+	foreach my $attrDef (@requiredAttributesDefinitions) {
+		# if attribute's namespace starts with "urn:perun:user:"
+		my $o = index $attrDef->getNamespace, "urn:perun:user:";
+		if ($o == 0) {
+			push @userRequiredAttributes, $attrDef;
+			next;
+		}
+		$o = index $attrDef->getNamespace, "urn:perun:user_facility:";
+		if ($o == 0) {
+			push @userFacilityRequiredAttributes, $attrDef;
+			next;
+		}
+	}
 	my @users;
 
 	####### prepare data ######################
-	foreach my $user (($data->getChildElements)[1]->getChildElements) {
+	my %usersIds = ();
+	foreach my $memberId ($data->getMemberIdsForFacility()) {
+		my $userId = $data->getUserIdForMember(member => $memberId);
+		if (exists($usersIds{$userId})) {
+			next;
+		} else {
+			$usersIds{$userId} = 0;
+		}
 		my $userData = {};
-		foreach my $uAttribute ($user->getAttributes) {
+
+		foreach my $userAttribute (@userRequiredAttributes) {
+			my $attrValue = $data->getUserAttributeValue(member => $memberId, attrName => $userAttribute->getName);
 			# In case there is an undefined boolean attribute, we have to change it to false
-			if ($uAttribute->getType eq "boolean" && !defined $uAttribute->getValue) {
-				$userData->{$uAttribute->getName} = \0;
+			if ($userAttribute->getType eq "boolean" && !defined $attrValue) {
+				$userData->{$userAttribute->getName} = \0;
 			} else {
-				$userData->{$uAttribute->getName} = $uAttribute->getValue;
+				$userData->{$userAttribute->getName} = $attrValue;
+			}
+		}
+
+		foreach my $userFacilityAttribute (@userFacilityRequiredAttributes) {
+			my $attrValue = $data->getUserFacilityAttributeValue(member => $memberId, attrName => $userFacilityAttribute->getName);
+			# In case there is an undefined boolean attribute, we have to change it to false
+			if ($userFacilityAttribute->getType eq "boolean" && !defined $attrValue) {
+				$userData->{$userFacilityAttribute->getName} = \0;
+			} else {
+				$userData->{$userFacilityAttribute->getName} = $attrValue;
 			}
 		}
 		push @users, $userData;
@@ -38,7 +78,7 @@ sub generateUsersDataInJSON {
 	####### output file ######################
 	my $fileName = "$DIRECTORY/$::SERVICE_NAME";
 	open FILE, ">$fileName" or die "Cannot open $fileName: $! \n";
-	print FILE JSON::XS->new->utf8->pretty->encode(\@users);
+	print FILE JSON::XS->new->utf8->pretty->canonical->encode(\@users);
 	close FILE or die "Cannot close $fileName: $! \n";
 
 	perunServicesInit::finalize;
