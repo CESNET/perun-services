f9af51a3efd9dccbf5063f09ecb4a8a6341c5870
Pavel Zlámal <zlamal@cesnet.cz>
2022-11-25 09:55:47 +0100

    fix(vsup_ifis): use lower-cased column names for postgres
    - Postgres is case-sensitive when processing column names.

--- send/VsupIfis.pm
+++ send/VsupIfis.pm
@@ -64,17 +64,18 @@ sub load_is() {
 	# Structure to store data from input database (IS)
 	my $inputData = {};
 	while(my $row = $sth->fetchrow_hashref()) {
-		my $key = $row->{VZTAH_CISLO};
-		$inputData->{$key}->{'OSB_ID'} = $row->{UCO};
-		$inputData->{$key}->{'TYP_VZTAHU'} = $row->{TYP_VZTAHU};
-		$inputData->{$key}->{'DRUH_VZTAHU'} = $row->{DRUH_VZTAHU};
-		$inputData->{$key}->{'STU_FORMA'} = $row->{STU_FORMA};
-		$inputData->{$key}->{'STUD_STAV'} = $row->{STUD_STAV};
-		$inputData->{$key}->{'STU_PROGR'} = $row->{STU_PROGR};
-		$inputData->{$key}->{'NS'} = $row->{NS};
-		$inputData->{$key}->{'VZTAH_OD'} = $row->{STUD_OD};
-		$inputData->{$key}->{'VZTAH_DO'} = $row->{STUD_DO};
-		$inputData->{$key}->{'KARTA_IDENT'} = $row->{KARTA_IDENT};
+		# disregarding the select Postgres is using lower-cased column names.
+		my $key = $row->{vztah_cislo};
+		$inputData->{$key}->{'OSB_ID'} = $row->{uco};
+		$inputData->{$key}->{'TYP_VZTAHU'} = $row->{typ_vztahu};
+		$inputData->{$key}->{'DRUH_VZTAHU'} = $row->{druh_vztahu};
+		$inputData->{$key}->{'STU_FORMA'} = $row->{stu_forma};
+		$inputData->{$key}->{'STUD_STAV'} = $row->{stud_stav};
+		$inputData->{$key}->{'STU_PROGR'} = $row->{stu_progr};
+		$inputData->{$key}->{'NS'} = $row->{ns};
+		$inputData->{$key}->{'VZTAH_OD'} = $row->{stud_od};
+		$inputData->{$key}->{'VZTAH_DO'} = $row->{stud_do};
+		$inputData->{$key}->{'KARTA_IDENT'} = $row->{karta_ident};
 	}
 
 	# Disconnect from input database (IS)
