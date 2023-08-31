3d1934ea035fc5804b88940641ba675d936ef07a
Pavel Zlámal <zlamal@cesnet.cz>
2023-08-31 19:35:37 +0200

    feat: optionally append BA credentials to URL destinations
    - If BA credentials are present in standardized service config file,
      append them to the CURL command like '-u user:pass'.
    - Supported in generic_send.py and generic_sender.py scripts when everything
      runs the standard way. If you override transport_command or create it
      by using generic_sender.prepare_url_transport_command() then no credentials
      are appended, and you have to do it by yourself.
    - We can't simply use overridden transport_command for this functionality,
      since it doesn't work for URL destinations correctly (can't catch stdout
      in temporary file).
    
    DEPLOYMENT NOTE:
      * All services using URL destination and standard send script will
        start append basic auth credentials if stored in their config
        at `/etc/perun/services/service_name/service_name.py`
      * Locally modified send scripts need to be checked and updated.
      * Configuration for `pithia_portal` service must be updated to not
        use default properties for authentication to token endpoint
        (username -> tokenUsername, password -> tokenPassword).

--- send/generic_sender.py
+++ send/generic_sender.py
@@ -187,6 +187,13 @@ def send(
         temp = tempfile.NamedTemporaryFile(mode="w+")
         # errors will be saved to temp file
         transport_command = prepare_url_transport_command(temp)
+        # append BA credentials if present in service config for the destination
+        auth = send_lib.get_auth_credentials(service_name, destination)
+        if auth is not None:
+            username = auth[0]
+            password = auth[1]
+            if username is not None and password is not None:
+                transport_command.extend(["-u", username + ":" + password])
     elif transport_command is None:
         transport_command = prepare_ssh_transport_command()
 
