664e017d918176a47f3c1673f1d616dd052f67d6
Pavel Břoušek <brousek@ics.muni.cz>
2024-01-17 12:22:35 +0100

    refactor: make crm_ceitec PSR-12 compliant

--- send/crm_ceitec.php
+++ send/crm_ceitec.php
@@ -2,23 +2,30 @@
 
 require '/etc/perun/services/crm_ceitec/crm_ceitec.php';
 
-$client = new SoapClient("https://wcf3.ceitec.cz/ExternalWWW.WCFService.svc?wsdl", array('login' => $l, 'password' => $p, 'trace' => 'true'));
+$client = new SoapClient(
+    "https://wcf3.ceitec.cz/ExternalWWW.WCFService.svc?wsdl",
+    array('login' => $l, 'password' => $p, 'trace' => 'true')
+);
 
 $longopts  = array(
-	"userName:",
-	"firstName:",
-	"lastName:",
-	"email:",
-	"orgUnit:",
-	"universityId:",
-	"eppn:",
-	"rgs:",
-	"alternativeLoginNames:",
+    "userName:",
+    "firstName:",
+    "lastName:",
+    "email:",
+    "orgUnit:",
+    "universityId:",
+    "eppn:",
+    "rgs:",
+    "alternativeLoginNames:",
 );
 
 $params = getopt("", $longopts);
 
-# public string[] CreateUser ( string userName, string firstName, string lastName, string email, string orgUnit, string universityId, string eppn , string rgs, string alternativeLoginNames )
+# public string[] CreateUser (
+    # string userName, string firstName, string lastName,
+    # string email, string orgUnit, string universityId,
+    # string eppn , string rgs, string alternativeLoginNames
+# )
 $response = $client->__soapCall("CreateUser", array($params));
 
 #if ($response->Valid === false) {
@@ -31,9 +38,7 @@ $response = $client->__soapCall("CreateUser", array($params));
 $array = $response->CreateUserResult->string;
 
 if ($array[0] == 0) {
-	print("SUCCESS");
+    print("SUCCESS");
 } else {
-	print("ERROR: " . $params["userName"] . " " . $array[1]);
+    print("ERROR: " . $params["userName"] . " " . $array[1]);
 }
-
-?>
